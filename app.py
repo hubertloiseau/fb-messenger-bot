a17e877ff8dadaa840ae6f5386f16ac676a8d8bd
ducdohb <ducdohb@users.noreply.github.com>
2017-01-04 16:16:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,13 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    send_message(sender_id, "loi me no roi")
+                    
+                    try:
+                        response = simSimi.getConversation(messaging_event["message"]["text"])
+                        print response['response']
+                    except SimSimiException as e:
+                        send_message(sender_id, "loi me no roi")
+                    send_message(sender_id, response['response'])
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
