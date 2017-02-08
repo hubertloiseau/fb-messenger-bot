0b5d2174cb3f7939e1c9182d0b8c9fcbc0f16c51
t988260 <huloiseau@airfrance.fr>
2017-02-08 15:51:14 +0100

    add greetings function

--- app.py
+++ app.py
@@ -106,6 +106,25 @@ def send_message(recipient_id, message_text):
         log(r.text)
 
 
+def send_greetings(greeting_text):
+	log("sending greetings")
+	params = {
+		"access_token": os.environ["PAGE_ACCESS_TOKEN"]
+	}
+	headers = {
+		"Content-Type": "application/json"
+	}
+        
+	data = json.dumps({
+	"greeting": {
+		"text": greeting_text
+		}
+	})
+	r = requests.post("https://graph.facebook.com/v2.6/me/thread_settings", params=params, headers=headers, data=data)
+	if r.status_code != 200:
+		log(r.status_code)
+		log(r.text)
+
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
     sys.stdout.flush()
